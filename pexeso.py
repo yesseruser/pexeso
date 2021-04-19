3aad514c156a837a3750733f2fa221335eb74dab
yesseruser <matej2.petricek@outlook.cz>
2021-04-19 18:23:12 +0200

    Added a list of cards named karticky and added type hints for all variables

--- pexeso.py
+++ pexeso.py
@@ -1,21 +1,35 @@
+from typing import Any, Tuple, Union, List
+
 import pygame
+from pygame.surface import Surface
 
 pygame.init()
 
-okno = pygame.display.set_mode((700, 700))
-
+okno: Surface = pygame.display.set_mode((700, 700))
+souradnice: tuple[Union[int, Any], int] = (sloupec * 150 + (sloupec + 1) * 20, 20)
 obrazky = []
 
 
 def nacteni_obrazku():
+    """
+    Loads all Pictures
+    """
     for index in range(0, 9, 1):
         pygame.transform.smoothscale(pygame.image.load("obrazky/" + str(index) + ".png"),(150, 150))
         obrazky.append(pygame.image.load("obrazky/" + str(index) + ".png"))
 
-
+karticky: list[list[int]] = [
+    [1, 2, 5, 7],
+    [5, 6, 3, 4],
+    [6, 7, 8, 3],
+    [8, 4, 2, 1]
+]
 def kresleni_karticek():
+    """
+    Draws all the cards in a line
+    """
     for sloupec in range(0, 4, 1):
-        okno.blit(obrazky[0], (sloupec * 150 + (sloupec + 1) * 20, 20))
+        okno.blit(obrazky[0], souradnice)
     pygame.display.flip()
 
 
