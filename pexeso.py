cb7436fe7d0fc9ed696d7daef67ddf75ef42966b
Yesseruser <matej2.petricek@outlook.cz>
2021-04-26 18:30:05 +0200

    Some changes

--- pexeso.py
+++ pexeso.py
@@ -34,6 +34,7 @@ def kresleni_karticek():
             souradnice: tuple[Union[int, Any], int] = (sloupec * 150 + (sloupec + 1) * 20, radek * 150 + (radek + 1) * 20)
             if prvni_karticka == (radek, sloupec):
                 okno.blit(obrazky[1], souradnice)
+                okno.blit(obrazky[kart])
             else:
                 okno.blit(obrazky[0], souradnice)
             okno.blit(obrazky[0], souradnice)
@@ -54,4 +55,5 @@ while run:
             print(mx, my)
             x, y = mx // 170, my // 170
             print(x, y)
+            prvni_karticka = x, y
         kresleni_karticek()
